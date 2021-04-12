6a67b04ba9f6ce28c8ab9b18994fedc66048e968
JB-X <61119185+jepribarus@users.noreply.github.com>
2021-04-12 22:47:26 +0700

    Add files via upload

---
+++ jepribarus/buatbot.js
@@ -0,0 +1,47 @@
+const buatbot = () => { 
+	return `
+	
+   ❒ CARA MEMBUAT BOT WA❒
+
+1. Download Termux Di Playstore
+
+•Ketik Cmd/Perintah Berikut Di Termux•
+👇👇👇👇
+
+pkg update && pkg upgrade
+
+apt-get update
+
+pkg install git bash nodejs imagemagick ffmpeg wget mc -y
+
+git clone https://github.com/jepribarus/bot
+
+cd bot
+
+bash install.sh
+
+npm i
+
+npm i imgbb-uploader
+
+node index.js
+
+2. Scane Kode Yang Muncul Di Termux Di Whatsapp Web
+
+3. Untuk Mematikan Bot ketik CTRL + Z
+
+4. Untuk Menghidupkan Bot Ketik
+cd bot
+node index.js
+
+⚠️NOTE : JIKA BELUM PAHAM CHAT SAYA :
+wa.me/12347590003
+
+‼️BOT HANYA WORK DI ANDROID VERSI 8 KE ATAS
+
+╭──━─── ❀۝❀ ───━──╮
+                   BOT JEPRI
+╰──━─── ❀۝❀ ───━──╯`
+}
+
+exports.buatbot = buatbot
